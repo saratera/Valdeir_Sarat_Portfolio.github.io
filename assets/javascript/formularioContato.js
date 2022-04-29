a20bf066025925887b51be65dc382938f246928a
saratera <valdeir.sarat@gmail.com>
2022-04-29 17:55:18 -0300

    validação do formulario

--- assets/javascript/formularioContato.js
+++ assets/javascript/formularioContato.js
@@ -1,21 +1,71 @@
 const nome = document.querySelector('[data-nome]');
 nome.addEventListener("blur", validarNome);
 
-function validarNome(){
-  const nomeDigitado = nome.value;
-  if(nomeDigitado.length < 3 || nomeDigitado == " "){
-        console.log('erro');
+function validarNome(event){
+  const nomeDigitado = nome.value.replace(/\s/g,'');
+  const aviso = document.querySelector('[data-nomeAviso]');
+  if(nomeDigitado.length == 0  || nomeDigitado.length < 3){
+        aviso.style.display = 'block'
+    }else{
+        aviso.style.display = 'none'
     }
     
 }
 
-
-
-
 const email = document.querySelector('[data-email]');
+email.addEventListener('blur', validarEmail)
+
+function validarEmail(event){
+    const emailCampo = email.value.replace(/\s/g,'');
+    const formatoEmail = /\S+@\S+\.\S+/;
+       
+    const emailAviso =  document.querySelector('[data-emailAviso]');
+    if(formatoEmail.test(emailCampo) != true || emailCampo.length == ' '){
+        
+        emailAviso.style.display = 'block'
+       
+    }else{
+        emailAviso.style.display = 'none'
+    }
+}
+
+
 const assunto = document.querySelector('[data-assunto]');
-const mensagem =document.querySelector('[data-mensagem]');
+assunto.addEventListener('blur', validarAssunto)
+
+function validarAssunto(event){
+    const assuntoCampo = assunto.value.replace(/\s/g,'');
+    const assuntoAviso = document.querySelector('[data-avisoAssunto]');
+    if(assuntoCampo.length == 0 ){
+        assuntoAviso.style.display = 'block'
+    }else{
+        assuntoAviso.style.display = 'none'
+    }
+}
+
+const mensagem = document.querySelector('[data-mensagem]');
+mensagem.addEventListener('blur', validarMensagem);
+
+
+function validarMensagem(event){
+    const campoMensagem = mensagem.value.replace(/\s/g,'');
+    const mensagemAviso = document.querySelector('[data-mensagemAviso]');
+    if(campoMensagem.length == 0){
+        mensagemAviso.style.display = 'block'
+    }else{
+        mensagemAviso.style.display = 'none'
+    }
+}
+
+
+
 const btnEnviar = document.querySelector('[data-BtnEnviar]');
+btnEnviar.addEventListener('click', enviarEmail)
+function enviarEmail(){
+    alert('oi')
+}
+   
+
 
 
 
